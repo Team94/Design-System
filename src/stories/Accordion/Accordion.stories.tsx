173029f8fb0d3a9f1a263b653a7d30a15fefffb3
geonwooPark <white0581@naver.com>
2024-06-05 15:28:56 +0900

    Chore: 스토리북 테스트

--- src/stories/Accordion/Accordion.stories.tsx
+++ src/stories/Accordion/Accordion.stories.tsx
@@ -60,7 +60,7 @@ export function Open() {
           Text...Dummy Text...Dummy Text...Dummy Text...Dummy Text...Dummy
           Text...Dummy Text...Dummy Text...Dummy Text...Dummy Text...Dummy
           Text...Dummy Text...Dummy Text...Dummy Text...Dummy Text...Dummy
-          Text...Dummy Text...
+          Text...Dummy Text
         </p>
       ),
     },
